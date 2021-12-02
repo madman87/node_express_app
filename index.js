c83d5db97a3437b6983675b21915956842a75ab1
Roman <itexpress@bk.ru>
2021-12-02 11:32:28 +0100

    rest plugin added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import Express from "express";
 
 const app =Express();
+const port = 3000;
 
-app.get('/', );
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+
+app.listen(port, () => console.log('listening on port'+port));
