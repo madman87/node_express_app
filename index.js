2fe6af8a38ce1ec309214d8139bd60962830704d
Roman <itexpress@bk.ru>
2021-12-02 20:02:06 +0100

    update

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ import Express from "express";
 
 const app =Express();
 const port = 3000;
+app.use(Express.json());
+app.use(Express.urlencoded({extended: true}));
 
-app.get('/', (req,res)=>{
-    res.send('Hello World');
+//GET, PUT, POST, DELETE
+
+app.get('/products/:id', (req,res)=>{
+    // res.send('Hello World');
+    res.json(Products.find((product) =>{
+        return +req.params.id===product.id;
+    }));
+});
+
+app.post('/add', (req,res)=>{
+    res.send(req.body);
 });
 
 app.listen(port, () => console.log('listening on port'+port));
